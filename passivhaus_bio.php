b875e5fadf5aab7ea94c1c1b628d0dddf66c5fbf
Nikko <manieto@gmail.com>
2016-03-14 21:40:19 +0100

    Mejorado el ajuste según resolución de las paǵinas de Promoción, Bioconstrucción y Passivhaus-Bio.

--- passivhaus_bio.php
+++ passivhaus_bio.php
@@ -13,12 +13,16 @@
 
 		<!-- News v3 Alternative -->
 		<div class="container content">
-			<h2 class="title-v2">OBRA NUEVA</h2>
-			<div class="row">
-				<div class="col-sm-6 col-md-5 col-lg-4 sm-margin-bottom-20">
+			<div class="row" class="padding-bottom-20">
+				<div class="col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
+					<h2 class="title-v2">OBRA NUEVA</h2>
+				</div>
+			</div>
+			<div class="row padding-bottom-20">
+				<div class="col-sm-6 col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
 					<img class="img-responsive" src="assets/img/proyecto_tierra.jpg" alt="">
 				</div>
-				<div class="col-sm-6 news-v3">
+				<div class="col-sm-6 col-md-7 col-lg-6 col-lg-offset-0 news-v3">
 					<div class="news-v3-in-sm no-padding">
 						<h2><a href="#">Proyecto Tierra</a></h2>
 						<p><span class="ecocreamos-logo">EcoCreamos</span> se honra en comunicar que el proyecto "Tierra" ha obtenido la primera Certificación de edificio PASSIVHAUS de las Illes Balears. Además de los más estrictos controles sobre eficiencia energética y aislamiento propios de los edificios de consumo casi nulo, "Tierra" es un proyecto realizado con criterios de bioconstrucción, sostenible y cuidado del medio ambiente. Siendo así, que ha sido la primera vivienda Certificada, realizada con BTC (bloque de tierra compactada), con tierra del mismo solar que se ha construido en España.</p>
@@ -29,13 +33,11 @@
 					</div>
 				</div>
 			</div>
-			<div class="row" style="padding: 20px;">
-			</div>
 			<div class="row">
-				<div class="col-sm-6 col-md-5 col-lg-4 sm-margin-bottom-20">
+				<div class="col-sm-6 col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
 					<img class="img-responsive" src="assets/img/proyecto_tierra.jpg" alt="">
 				</div>
-				<div class="col-sm-6 news-v3">
+				<div class="col-sm-6 col-md-7 col-lg-6 col-lg-offset-0 news-v3">
 					<div class="news-v3-in-sm no-padding">
 						<h2><a href="#">Mancor de la Vall</a></h2>
 						<p>Vivienda unifamiliar aislada, construida con criterios Passivhaus-Bio. Es la primera vivienda construida en las Illes Balears en la que Bioconstrucción y la eficiencia energética del estándar Passivhaus se unen, obteniendo un proyecto altamente eficiente y ecológico.</p>
@@ -49,12 +51,16 @@
 		<!-- End News v3 Alternative -->
 		<!-- News v3 Alternative -->
 		<div class="container content" style="padding-top: 0px;">
-			<h2 class="title-v2">REHABILITACIÓN</h2>
+			<div class="row" class="padding-bottom-20">
+				<div class="col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
+					<h2 class="title-v2">REHABILITACIÓN</h2>
+				</div>
+			</div>
 			<div class="row">
-				<div class="col-sm-6 col-md-5 col-lg-4 sm-margin-bottom-20">
+				<div class="col-sm-6 col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
 					<img class="img-responsive" src="assets/img/proyecto_tierra.jpg" alt="">
 				</div>
-				<div class="col-sm-6 news-v3">
+				<div class="col-sm-6 col-md-7 col-lg-6 col-lg-offset-0 news-v3">
 					<div class="news-v3-in-sm no-padding">
 						<h2><a href="#">Santanyí</a></h2>
 						<p><span class="ecocreamos-logo">EcoCreamos</span> realiza la primera rehabilitación ecológica aplicando los conceptos del estándar Passivhaus en este municipio. La rehabilitación se ha realizado respetando la tipología y el carácter de la construcción existente en sus fachadas exteriores, e integrando el sistema de recuperación de aire y las instalaciones en el interior de la vivienda.</p>
--- promocion_ecologica.php
+++ promocion_ecologica.php
@@ -13,25 +13,29 @@
 		
 		<div class="container content">
 			<div class="row">
-				<div class="col-sm-6 sm-margin-bottom-20">
+				<div class="col-md-7 col-lg-6 col-lg-offset-1 sm-margin-bottom-20">
 					<p>Las ventajas de una Construcción Ecológica se pueden concretar en un <strong>Ahorro Económico y Respeto del Medioambiente</strong>, al reducirse las emisiones responsables del cambio climático.</p>
 					<p>Nuestra empresa busca con la promoción ecológica, ofrecer al mercado inmobiliario la posibilidad de obtener viviendas sostenibles y ecointeligentes, ofreciendo proyectos, donde la integración geobiológica de la vivienda, la elección de los materiales y fuentes de energía, además de una estudiada geometría aplicada a través de la arquitectura bioenergética.</p>
 					<p>Conseguimos en cada proyecto un espacio donde es posible desde el primer instante percibir una sensación de vitalidad y bienestar, demostrando que todo ello no esta ligado a un coste superior comparándolo con un proyecto convencional.</p>
 					<p>Visite la obra que actualmente estamos <strong>construyendo en Llucmajor</strong>. Puede llamarnos para concretar una visita.</p>
 				</div>
-				<div class="col-sm-6 col-md-5 col-lg-4">
+				<div class="col-md-5 col-lg-4">
 					<img src="assets/img/proyecto_tierra.jpg" class="img-responsive margin-bottom-10" alt="">
 				</div>
 			</div>
 		</div>
 		<!-- News v3 Alternative -->
 		<div class="container content" style="padding-top: 0px;">
-			<h2 class="title-v2">OBRA NUEVA</h2>
 			<div class="row">
-				<div class="col-sm-6 col-md-5 col-lg-4 sm-margin-bottom-20">
+				<div class="col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
+					<h2 class="title-v2">OBRA NUEVA</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 col-md-5 col-lg-4 col-lg-offset-1 sm-margin-bottom-20">
 					<img class="img-responsive" src="assets/img/proyecto_tierra.jpg" alt="">
 				</div>
-				<div class="col-sm-6 news-v3">
+				<div class="col-sm-6 col-md-7 col-lg-6 news-v3">
 					<div class="news-v3-in-sm no-padding">
 						<h2><a href="#">Proyecto Tierra</a></h2>
 						<p><span class="ecocreamos-logo">EcoCreamos</span> se honra en comunicar que el proyecto "Tierra" ha obtenido la primera Certificación de edificio PASSIVHAUS de las Illes Balears. Además de los más estrictos controles sobre eficiencia energética y aislamiento propios de los edificios de consumo casi nulo, "Tierra" es un proyecto realizado con criterios de bioconstrucción, sostenible y cuidado del medio ambiente. Siendo así, que ha sido la primera vivienda Certificada, realizada con BTC (bloque de tierra compactada), con tierra del mismo solar que se ha construido en España.</p>
