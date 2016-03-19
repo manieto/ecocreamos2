9ce8194b6e99370ec8e6a3dc63242397303244d9
Nikko <manieto@gmail.com>
2016-03-19 20:36:42 +0100

    Empezando con Naturhaus...

--- material_naturhaus.php
+++ material_naturhaus.php
@@ -14,14 +14,46 @@
 		
 		<div class="container content">
 			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1 col-md-6 col-lg-6 padding-bottom-20">
-					<p><span class="ecocreamos-logo">EcoCreamos</span> es distribuidor exclusivo de los productos <a href="http://www.ecoclay.es/" target="ecoclay">Ecoclay</a> en las Illes Balears.</p>
-					<p>aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa, aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa, aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa, aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa.</p>
-					<p>aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa, aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa, aaaaa aaaa aaaaaa aa aaaa a aaaaaaaaaaaaa aaaa aaaaaa aa aaaa a aaaaaaaa aaaaa aaaa aaaaaa aa aaaa a aaaaaaaa.</p>
+				<div class="col-md-7 padding-bottom-20 padding-left-30 padding-right-30">
+					<p><span class="ecocreamos-logo">EcoCreamos</span> es distribuidor exclusivo de los productos <a href="http://naturhaus.net/" target="naturhaus">Naturhaus</a> en las Illes Balears.</p>
+					<p><em>"Los materiales naturales se tratan de forma natural."</em></p>
+					<p>¿Le importa que las superficies de madera tengan el máximo de su textura natural?</p>
+					<p>Nosotros sabemos lo que necesita su madera. Los productos NATURHAUS protegen, conservan, limpian, cuidan y reavivan la superficie de madera de forma absolutamente adecuada; esto quiere decir de manera totalmente natural (como la propia madera).</p>
+					<p>Fieles a nuestra premisa "Los materiales naturales se tratan de forma natural", utilizamos únicamente materias primas renovables en la elaboración de nuestros productos y esto a su madera le sienta bien. Ya sea para exteriores o interiores, madera clara o madera oscura, para suelos o para muebles. Tenemos el producto idóneo para cada exigencia.</p>
 				</div>
-				<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-0 col-lg-4">
-					<a href="http://www.ecoclay.es/" target="ecoclay"><img src="assets/img/logos/ecoclay.gif" class="img-responsive margin-bottom-10" alt="Ecoclay" /></a>
+				<div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 col-md-offset-1">
+					<a href="http://naturhaus.net/" target="naturhaus"><img src="assets/img/logos/naturhaus.jpg" class="img-responsive margin-bottom-10" alt="Naturhaus" /></a>
 				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-xs-12 padding-left-30">
+					<h2 class="title-v2 padding-top-20">Procesos de aplicación</h2>
+					<div class="col-sm-6">
+						<ul class="list-unstyled who">
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/tratamiento_de_suelos_interior_naturhaus.pdf"       target="naturhaus">Tratamiento - Suelos de interior</a></li>
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/tratamiento_de_suelos_exterior_naturhaus.pdf"       target="naturhaus">Tratamiento - Suelos de exterior</a></li>
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/tratamiento_maderas_al_exterior_naturhaus.pdf"      target="naturhaus">Tratamiento - Madera al exterior</a></li>
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/tratamiento_de_muebles_o_elementos_decorativos.pdf" target="naturhaus">Tratamiento - Maderas al interior</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-6">
+						<ul class="list-unstyled who">
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/mantenimiento_suelos_interior_tratados.pdf"       target="naturhaus">Mantenimiento - Suelos de interior</a></li>
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/web_mantenimiento_suelos_exterior_tratados.pdf"   target="naturhaus">Mantenimiento - Suelos de exterior</a></li>
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/mantenimiento_de_muebles_de_jardin_de_madera.pdf" target="naturhaus">Mantenimiento - Mobiliario de jardín</a></li>
+							<li><i class="fa fa-file-pdf-o"></i> <a href="assets/pdf/material_naturhaus/mantenimiento_maderas_al_exterior_naturhaus.pdf"  target="naturhaus">Mantenimiento - Maderas al exterior</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-xs-12 padding-left-30">
+					<h2 class="title-v2 padding-top-20">Productos</h2>
+				</div>
+				Lista de productos con despliegue tipo acordeón múltiple.
+				¿Cambiar todos los target a _blank para evitar que una pestaña se actualice sin darnos cuenta?
 			</div>
 		</div>
 
