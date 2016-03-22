d758714889fc49c0cf9da2ada6d04dd82b5f25fc
Nikko <manieto@gmail.com>
2016-03-22 02:03:21 +0100

    Noticias acabadas. Otras mejoras.

--- producto_biomasa.php
+++ producto_biomasa.php
@@ -14,7 +14,7 @@
 		
 		<div class="container content">
 			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1 sm-margin-bottom-20">
+				<div class="col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1 col-lg-6 sm-margin-bottom-20">
 					<p><span class="ecocreamos-logo">EcoCreamos</span> es distribuidor oficial en Mallorca de los productos <a target="_blank" href="http://www.lacunza.net/">Lacunza</a>.</p>
 					<ul class="list-unstyled who">
 						<li><a target="_blank" href="http://www.lacunza.net/es/cocinas/"><i class="fa fa-leaf"></i>Cocinas</a></li>
@@ -24,7 +24,7 @@
 						<li><a target="_blank" href="http://www.lacunza.net/es/accesorios/"><i class="fa fa-check"></i>Accesorios</a></li>
 					</ul>
 				</div>
-				<div class="col-sm-5 col-lg-4 col-lg-offset-1">
+				<div class="col-sm-5 col-lg-4">
 					<p></p>
 					<a target="_blank" href="http://www.lacunza.net/"><img src="assets/img/logos/lacunza.png" class="img-responsive margin-bottom-10" alt="Acceso a la web de Lacunza"></a>
 				</div>
