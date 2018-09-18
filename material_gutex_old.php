79c3502fa3fcd8d6bf4463bae28e4cba109d8ae2
Miguel Ángel Nieto <manieto@gmail.com>
2018-09-18 21:11:02 +0200

    Actualizamos el repositorio con los últimos cambios que se hicieron en la web hace ya unos cuantos meses...

--- material_gutex_old.php
+++ material_gutex_old.php
@@ -21,7 +21,7 @@
 		<div class="container content">
 			<div class="row">
 				<div class="col-md-7 padding-left-30 padding-right-30 padding-bottom-20">
-					<p><span class="ecocreamos-logo-sm">EcoCreamos</span> es distribuidor exclusivo de los productos <a href="http://gutex.es/" target="_blank">Gutex</a> en las Illes Balears.</p>
+					<p><span class="ecocreamos-logo-sm">EcoCreamos</span> es distribuidor exclusivo de los productos <a href="http://gutex-espana.eu/" target="_blank">Gutex</a> en las Illes Balears.</p>
 					<p>Los tableros de fibra de madera alemanes <em>Gutex</em> responden a las mayores exigencias en materia de aislamiento: es ecológico y económico en todas sus aplicaciones. </p>
 					<p>Sus ventajas en el campo de la física de la construcción dan como resultado un agradable clima interior: la estructura porosa de sus fibras, favorece la difusión de vapor; los tableros "respiran", evitan el efecto de pared fría y regulan la humedad. Gracias a su estructura de poros abiertos son capaces de absorber las ondas sonoras. Del mismo modo, también mejoran considerablemente la amortiguación del ruido de impacto.</p>
 					<p>Su excelente protección frente al calor estival y el frío invernal distingue a los tableros de fibra de madera <em>Gutex</em> de los demás aislantes. Su inercia térmica (capacidad de absorción de calor) es la más alta de todos los aislantes. Los tableros de fibra de madera son totalmente reciclables y compostables, por lo que no producen residuos.</p>
@@ -29,7 +29,7 @@
 					<a href="assets/pdf/material_gutex/GUTEX_ES_BR_Aplicacion_2015-06.pdf" class="btn-u btn-u-large" target="_blank">Catálogo y aplicaciones</a>
 				</div>
 				<div class="col-md-5 col-lg-offset-1 col-lg-4">
-					<a href="http://gutex.es/" target="_blank"><img src="assets/img/logos/gutex.png" class="img-responsive padding-bottom-20" alt="Gutex" /></a>
+					<a href="http://gutex-espana.eu/" target="_blank"><img src="assets/img/logos/gutex.png" class="img-responsive padding-bottom-20" alt="Gutex" /></a>
 					<img src="assets/img/materiales/gutex_interior.jpg" class="img-responsive padding-bottom-20" alt="Gutex" />
 				</div>
 			</div>
@@ -59,12 +59,12 @@
 				<div class="col-xs-12 padding-left-30 padding-right-30 padding-bottom-20">
 					<h2 class="title-v2 padding-top-20">Accesorios</h2>
 					<ul class="list-unstyled who">
-						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex.es/gama-de-productos/accesorios/fijacion-tirafondos/" target="_blank">Fijación / Tirafondos (12 elementos)</a></li>
-						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex.es/gama-de-productos/accesorios/perfileria-listones/" target="_blank">Perfilería / Listones (19 elementos)</a></li>
-						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex.es/gama-de-productos/accesorios/sellado-adhesivo/"    target="_blank">Sellado / Adhesivo (8 elementos)</a></li>
-						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex.es/gama-de-productos/accesorios/revoque-pintura/"     target="_blank">Revoque / Pintura (8 elementos)</a></li>
-						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex.es/gama-de-productos/accesorios/herramienta/"         target="_blank">Herramienta (2 elementos)</a></li>
-						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex.es/gama-de-productos/accesorios/varios/"              target="_blank">Varios (6 elementos)</a></li>
+						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex-espana.eu/gama-de-productos/accesorios/fijacion-tirafondos/" target="_blank">Fijación / Tirafondos (12 elementos)</a></li>
+						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex-espana.eu/gama-de-productos/accesorios/perfileria-listones/" target="_blank">Perfilería / Listones (19 elementos)</a></li>
+						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex-espana.eu/gama-de-productos/accesorios/sellado-adhesivo/"    target="_blank">Sellado / Adhesivo (8 elementos)</a></li>
+						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex-espana.eu/gama-de-productos/accesorios/revoque-pintura/"     target="_blank">Revoque / Pintura (8 elementos)</a></li>
+						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex-espana.eu/gama-de-productos/accesorios/herramienta/"         target="_blank">Herramienta (2 elementos)</a></li>
+						<li><i class="fa fa-file-pdf-o"></i> <a href="http://gutex-espana.eu/gama-de-productos/accesorios/varios/"              target="_blank">Varios (6 elementos)</a></li>
 					</ul>
 				</div>
 			</div>
